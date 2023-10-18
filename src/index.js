d88c78831b15a6ec1a638c9a99cbf1a86565ec67
Tommy Grullón Contreras <tommycontreras34@gmail.com>
2023-10-18 15:22:48 -0400

    Added functionality to save a file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 const express = require('express');
+const multer = require('multer');
 const app = express();
 
-console.log('Hello World')
+const upload = multer({ dest: 'uploads/' });
+
+app.post('/api/fileanalyze', upload.single('upfile'), function(req, res) {
+    res.json({
+        name: req.file.originalname,
+        type: req.file.mimetype,
+        size: req.file.size
+    });
+});
 
 app.listen(3000);
